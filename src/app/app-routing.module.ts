470ea8f2ccff3d283a7ac0a144da826232232f67
UlianaTelyuk <telyuk.uliana@gmail.com>
2018-12-18 19:38:43 +0200

    Created reset password page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageLoginComponent } from './page-login/page-login.component';
+import { PageResetPasswordComponent } from './page-reset-password/page-reset-password.component';
+import { PageProfileComponent } from './page-profile/page-profile.component';
 import { PageIndexComponent } from './page-index/page-index.component';
 
 const routes: Routes = [
   { path: 'login', component: PageLoginComponent },
+  { path: 'profile', component: PageProfileComponent },
+  { path: 'reset-password', component: PageResetPasswordComponent },
   { path: '', component: PageIndexComponent },
   { path: '**', redirectTo: '/' }
 ];
