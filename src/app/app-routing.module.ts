c8180741ea7c1e9cc96fcbf754586eec9bee31ad
UlianaTelyuk <telyuk.uliana@gmail.com>
2018-12-21 18:10:17 +0200

    Created profile page and profile edit page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { PageLoginComponent } from './page-login/page-login.component';
 import { PageResetPasswordComponent } from './page-reset-password/page-reset-password.component';
 import { PageProfileComponent } from './page-profile/page-profile.component';
+import { PageProfileEditComponent } from './page-profile-edit/page-profile-edit.component';
 import { PageIndexComponent } from './page-index/page-index.component';
 
 const routes: Routes = [
   { path: 'login', component: PageLoginComponent },
   { path: 'profile', component: PageProfileComponent },
   { path: 'reset-password', component: PageResetPasswordComponent },
+  { path: 'profile-edit', component: PageProfileEditComponent },
   { path: '', component: PageIndexComponent },
   { path: '**', redirectTo: '/' }
 ];
